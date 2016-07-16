129ee74555733565f0477ac9c79afed0129eace1
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:21 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- CampaignChainLocationFacebookBundle.php
+++ CampaignChainLocationFacebookBundle.php
@@ -2,8 +2,13 @@
 
 namespace CampaignChain\Location\FacebookBundle;
 
+use CampaignChain\Location\FacebookBundle\DependencyInjection\CampaignChainLocationFacebookExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CampaignChainLocationFacebookBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new CampaignChainLocationFacebookExtension();
+    }
 }
