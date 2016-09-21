6883da51ec2a91e63c78f89330a53554476c3184
Sandro Groganz <sandro@campaignchain.com>
2016-09-21 13:13:29 +0200

    CampaignChain/campaignchain#227 Undefined index: likes in location-facebook/Job/ReportFacebookPageMetrics.php on line 59

--- Job/ReportFacebookPageMetrics.php
+++ Job/ReportFacebookPageMetrics.php
@@ -10,14 +10,21 @@
 
 namespace CampaignChain\Location\FacebookBundle\Job;
 
+use CampaignChain\Channel\FacebookBundle\REST\FacebookClient;
 use CampaignChain\CoreBundle\Entity\SchedulerReportLocation;
+use CampaignChain\CoreBundle\EntityService\FactService;
 use CampaignChain\CoreBundle\Job\JobReportInterface;
+use CampaignChain\Location\FacebookBundle\Entity\LocationBase;
+use CampaignChain\Location\FacebookBundle\Entity\Page;
 use Doctrine\ORM\EntityManager;
+use CampaignChain\CoreBundle\Exception\ExternalApiException;
 
 class ReportFacebookPageMetrics implements JobReportInterface
 {
     const BUNDLE_NAME = 'campaignchain/location-facebook';
     const METRIC_LIKES = 'Likes';
+    const METRIC_FANS = 'Fans';
+    const METRIC_FRIENDS = 'Friends';
 
     protected $em;
     protected $container;
@@ -45,31 +52,47 @@ class ReportFacebookPageMetrics implements JobReportInterface
 
     public function execute($locationId)
     {
+        /** @var LocationBase $facebookLocation */
+        $facebookLocation = $this->em->getRepository('CampaignChainLocationFacebookBundle:LocationBase')
+            ->findOneByLocation($locationId);
+
+        /** @var FacebookClient $client */
         $client = $this->container->get('campaignchain.channel.facebook.rest.client');
-        $location = $this->em->getRepository('CampaignChainCoreBundle:Location')->find($locationId);
-        $connection = $client->connectByLocation($location);
+        $connection = $client->connectByLocation($facebookLocation->getLocation());
 
         if ($connection) {
+            $facts = array();
+            $this->message = 'Added to report:';
+            $baseUri = '/'.$facebookLocation->getIdentifier();
+            try {
+                if ($facebookLocation instanceof Page) {
+                    // Collect stats for Facebook Page Location.
+                    $response = $connection->api($baseUri, 'GET');
+                    $facts[self::METRIC_FANS] = $response['likes'];
+                    $this->message .= ' '.self::METRIC_FANS.' = '.$facts[self::METRIC_FANS];
+                } else {
+                    // Collect stats for Facebook User Location.
+                    $params = [
+                        'summary' => 'true',
+                    ];
+                    $response = $connection->api($baseUri . '/friends', 'GET', $params);
+                    $facts[self::METRIC_FRIENDS] = $response['summary']['total_count'];
+                    $this->message .= ' '.self::METRIC_FRIENDS.' = '.$facts[self::METRIC_FRIENDS];
+                }
 
-            // NOTE: this has already changed with FB API 2.6
-            $params = [
-                'fields' => 'likes',
-            ];
-            $response = $connection->api('/'.$location->getIdentifier(), 'GET', $params);
-            $likes = $response['likes'];
-
-            $connection->destroySession();
+                $connection->destroySession();
+            } catch (\Exception $e) {
+                throw new ExternalApiException($e->getMessage(), $e->getCode(), $e);
+            }
         } else {
             return self::STATUS_ERROR;
         }
 
-        // Add report data.
-        $facts[self::METRIC_LIKES] = $likes;
-
-        $factService = $this->container->get('campaignchain.core.fact');
-        $factService->addLocationFacts(self::BUNDLE_NAME, $location, $facts);
-
-        $this->message = 'Added to report: likes = '.$likes.'.';
+        if(count($facts)) {
+            /** @var FactService $factService */
+            $factService = $this->container->get('campaignchain.core.fact');
+            $factService->addLocationFacts(self::BUNDLE_NAME, $facebookLocation->getLocation(), $facts);
+        }
 
         return self::STATUS_OK;
     }
